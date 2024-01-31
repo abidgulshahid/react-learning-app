36643b851cf7e6c8698626a0b59e8ba19a58356e
Abid Gul Shahid <abidgulshahid@gmail.com>
2024-01-31 09:34:12 +0500

    Update ListGroups.tsx

--- react_learning/src/Components/ListGroups.tsx
+++ react_learning/src/Components/ListGroups.tsx
@@ -1,11 +1,17 @@
 
 function ListGroups(){
     const Lists  = ['Abid', 'Gul', 'Shahid', 'Marwat']
+    var items = []
+    const getItems = () => {
+        return items.length === 0 && <p>No item found</p>
+    }
   
     return (
         <>
+        {getItems()}
     <h1>List</h1>
 <ul className="list-group">
+
     {Lists.map((items) => (<li key={items}>{items}</li>))}
 </ul>
 </>
