84fc344f56e17813e5fa04f21a494d958043d8b9
Abid Gul Shahid <abidgulshahid@gmail.com>
2024-01-31 09:40:47 +0500

    Update ListGroups.tsx

--- react_learning/src/Components/ListGroups.tsx
+++ react_learning/src/Components/ListGroups.tsx
@@ -5,6 +5,12 @@ function ListGroups(){
     const getItems = () => {
         return items.length === 0 && <p>No item found</p>
     }
+    let count = 0
+    const counter = () => {
+        count = count + 1
+        return count
+
+    }
   
     return (
         <>
@@ -12,7 +18,7 @@ function ListGroups(){
     <h1>List</h1>
 <ul className="list-group">
 
-    {Lists.map((items) => (<li key={items}>{items}</li>))}
+    {Lists.map((items) => (<li onClick={() => console.log(items)} className="list-group-item" key={items}>{items}</li>))}
 </ul>
 </>
 
